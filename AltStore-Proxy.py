1c890f3e38ce402e89f8ed83ff4cf0792f03f4c5
rix <rix@rix>
2024-05-19 21:05:07 +0200

    v.1.2.4 - Windows fix

--- AltStore-Proxy.py
+++ AltStore-Proxy.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
+# AltStore-Proxy
+# Project by https://github.com/rix1337
+
+import multiprocessing
+
 from altstore_proxy import run
 
 if __name__ == '__main__':
+    multiprocessing.freeze_support()
     run.main()
